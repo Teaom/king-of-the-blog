02b5581c2ba937677beccdd80c7324616cbfc716
Teaom <dclarkjenkins87@gmail.com>
2023-05-25 02:33:44 -0600

    update

--- models/index.js
+++ models/index.js
@@ -3,13 +3,24 @@ const Post = require('./Post');
 const Comment = require('./Comment');
 
 
-User.hasMany(Post, {
-  foreignKey: 'user_id',
+// User.hasMany(Post, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// });
+
+Post.belongsTo(User, {
+  foreignKey: 'userId',
   onDelete: 'CASCADE'
 });
 
-Post.belongsTo(User, {
-  foreignKey: 'user_id'
-});
+Comment.belongsTo(User, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+})
+
+Post.hasMany(Comment, {
+  foreignKey: 'postId',
+  onDelete: 'CASCADE'
+})
 
 module.exports = { User, Post, Comment };
